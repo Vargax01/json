30b038ca068447041c085c7147ae4b756714d95d
vargax <miguelchico14@gmail.com>
2017-03-07 19:59:03 +0100

    mejorado ej1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,5 +1,28 @@
 # -*- coding: utf-8 -*-
 import json
+print "Monóxido de nitrógeno"
+print "Dióxido de nitrógeno"
+print "Partículas en suspensión PM10"
+print "Ozono"
+print "Tolueno"
+print "Benceno"
+print "Xileno"
+print "Hidrocarburos totales"
+print "Hidrocarburos no metano"
+listadat=[]
+pasa=False
+while pasa == False:
+    dato=str(raw_input("Dame un dato pulse o pulse q para quitar"))
+    if dato == "q":
+        pasa=True
+    else:
+        listadat.append(dato.decode('utf-8'))
+
+
+cad_uni='Monóxido de nitrógeno'
 with open('contaminacion.json') as data_file:
     data = json.load(data_file)
-print data
+for elem in data:
+    print elem[cad_uni.decode('utf-8')]
+
+print listadat
