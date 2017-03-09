2bba59729e3edb10f54c7d9797fbf4b8490c8479
vargax <miguelchico14@gmail.com>
2017-03-09 18:49:26 +0100

    Todo terminado

--- ejercicio5.py
+++ ejercicio5.py
@@ -2,6 +2,24 @@
 import json
 with open('contaminacion.json') as data_file:
     data = json.load(data_file)
-
+print "Monóxido de nitrógeno"
+print "Dióxido de nitrógeno"
+print "Partículas en suspensión PM10"
+print "Ozono"
+print "Tolueno"
+print "Benceno"
+print "Xileno"
+print "Hidrocarburos totales"
+print "Hidrocarburos no metano"
 dato=str(raw_input("Dame un dato: "))
 dato=dato.decode('utf-8')
+valor=False
+print "Fechas en el que", dato, "este en null"
+for elem in data:
+    for elem1 in elem:
+        if elem1 == dato and elem[elem1] == None:
+            valor=True
+            print elem["Fecha"]
+
+if valor == False:
+    print "No hay fechas en el que", dato, "este en null"
